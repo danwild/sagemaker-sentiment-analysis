800f3ea6b7c06badc0ad8cb48c7fe61f8a04c45c
EC2 Default User <ec2-user@ip-172-16-24-99.ap-southeast-2.compute.internal>
2019-07-12 03:28:09 +0000

    save notebook and train/predict scripts

--- serve/predict.py
+++ serve/predict.py
@@ -65,13 +65,12 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    # Process input_data so that it is ready to be sent to our model.
+    #   You should produce two variables:
+    #     - data_X   - A sequence of length 500 which represents the converted review
+    #     - data_len - The length of the review
+    input_words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +83,12 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
+    # Compute the result of applying the model to the input data. The variable `result` should
+    # be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():
+        output = model.forward(data) 
 
-    result = None
-
-    return result
+    result = np.round(output.numpy())
+    print('result is: %s', result)
+    
+    return int(result)
